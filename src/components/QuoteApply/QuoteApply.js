c954647089df0736b861cba371506c3e6f144ab3
nitinpunnen <nitinpunnen@gmail.com>
2023-02-15 12:05:03 -0800

    added stepper

--- src/components/QuoteApply/QuoteApply.js
+++ src/components/QuoteApply/QuoteApply.js
@@ -1,16 +1,79 @@
-import React from "react";
-import "./QuoteApply.css";
-import "@aws-amplify/ui-react/styles.css";
-import {
-    View,
-} from '@aws-amplify/ui-react';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Text, Flex } from '@aws-amplify/ui-react';
 
-const QuoteApply = () => {
+const steps = ['Your Name', 'Gender', 'Your Health', 'Substance Use', 'Height and Weight', 'Date of Birth', 'Select your Policy Type'];
+
+export default function QuoteApply() {
+    const [activeStep, setActiveStep] = React.useState(0);
+    
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };    
+
+    const handleReset = () => {
+        setActiveStep(0);
+    };
+
     return (
-        <View>
-            <span>Quotes</span>
-        </View>
-    );
-};
+        <Flex
+            direction={{ base: 'column', large: 'column' }}
+            padding="1rem"
+            width="80%"
+            style={{ display: "block", margin: "10px auto" }}
+        >
+            <Text style={{ flexGrow: 3, color: "#F56600", fontSize: "24px", marginBottom: "20px" }}>Quote & Apply</Text>
+            <Box sx={{ width: '100%' }}>
+                <Stepper activeStep={activeStep} alternativeLabel>
+                    {steps.map((label, index) => {
+                        const stepProps = {};
+                        const labelProps = {};
+                        return (
+                            <Step key={label} {...stepProps}>
+                                <StepLabel {...labelProps}>{label}</StepLabel>
+                            </Step>
+                        );
+                    })}
+                </Stepper>
+                {activeStep === steps.length ? (
+                    <React.Fragment>
+                        <Typography sx={{ mt: 2, mb: 1 }}>
+                            All steps completed - you&apos;re finished
+                        </Typography>
+                        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                            <Box sx={{ flex: '1 1 auto' }} />
+                            <Button onClick={handleReset}>Reset</Button>
+                        </Box>
+                    </React.Fragment>
+                ) : (
+                    <React.Fragment>
+                        <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
+                        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                            <Button
+                                color="inherit"
+                                disabled={activeStep === 0}
+                                onClick={handleBack}
+                                sx={{ mr: 1 }}>
+                                Back
+                            </Button>
+                            <Box sx={{ flex: '1 1 auto' }} />
 
-export default QuoteApply;
+                            <Button onClick={handleNext}>
+                                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                            </Button>
+                        </Box>
+                    </React.Fragment>
+                )}
+            </Box>
+        </Flex>
+    );
+}
