6b199a8abb97855be14318019d79512cacdcbac2
nitinpunnen <nitinpunnen@gmail.com>
2023-02-27 14:20:45 -0800

    dayjs package

--- src/components/QuoteApply/ReviewSign.js
+++ src/components/QuoteApply/ReviewSign.js
@@ -2,14 +2,16 @@ import React from "react";
 import "@aws-amplify/ui-react/styles.css";
 import { Flex } from "@aws-amplify/ui-react";
 import SingleUserAttributes from "./SingleUserAttributes";
+import { useTranslation } from "react-i18next";
 
 const ReviewSign = (props) => {
     let userAttrs = props.userAttrs;
+    const { t, i18n } = useTranslation();
 
     return (
         <Flex
             direction={{ base: 'column', large: 'column' }}>
-            <span className="page-intro">Let's review the information you provided before we continue</span>
+            <span className="page-intro">{t('reviewSign.intro')}</span>
             <SingleUserAttributes userAttrs={userAttrs} />
         </Flex>
     );
